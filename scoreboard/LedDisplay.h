2d70882d41f2aaacb2f835795c5aac22fc2d4c84
jonasitzmann <jonasitzmann@hotmail.de>
2018-02-28 13:14:29 +0100

    improve showNumber()

--- scoreboard/LedDisplay.h
+++ scoreboard/LedDisplay.h
@@ -24,34 +24,32 @@ class LedDisplay {
     }
 
 /*
-* shows the value of digit on the position given by offset
+* shows the value on the position given by digitOffset
+*example with 4 digits: showDigit(7, 1) -> |0|0|7|0|
 */
-    void showDigit(int digit, int offset) {
-      if (digit < 0 || digit > 9) {
-        Serial.println("illegal argument");
-        return;
-      }
-      for (int pixel = 0; pixel < numPixels; ++pixel) {
-        int segment = pixel / 5;
-        bool pixelActive = digits[digit][segment];
-        int pixelBrightness = pixelActive ? brightness : 0;
+    void showDigit(int value, int digitOffset; int color[3] = {100, 100, 100}) {
+        int pixelOffset = digitOffset * 35;
         
-        // shift color (red -> green -> blue -> red) after each pixel
-        int redval = pixel % 3 == 0 ? pixelBrightness : 0;
-        int greenval = pixel % 3 == 1 ? pixelBrightness : 0;
-        int blueval = pixel % 3 == 2 ? pixelBrightness : 0;
-        lights->setPixelColor(pixel, redval, greenval, blueval);
+        if (digit < 0 || digit > 9) {
+            Serial.println("illegal argument");
+            return;
+      }
+      for (int pixel = offset; pixel < offset + 35; ++pixel) {
+        int segment = (pixel-pixelOffset) / 5;
+        bool pixelActive = digits[value][segment];
+        if(pixelActive){
+            lights->setPixelColor(pixel, color);
+        }
+        else{
+            lights->setPixelColor(pixel, 0);
       }
     }
     void showNumber(int number){
-        int digit;
-        int offset = 0;
-        do{
-            digit = number % 10;
-            showDigit(digit, offset);
-            offset += 35;
-            number /= 10;
-        } while (number != 0);
+        int numDigits = numPixels / 35;
+        for (int i=0; i<numPixels; ++i){
+            int digitFactor = pow(10, (numPixels-1-i));
+            lights->showDigit(number%digitFactor, i);
+        }
         lights->show();
     }
 
